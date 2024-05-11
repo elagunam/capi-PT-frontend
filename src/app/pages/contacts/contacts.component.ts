4b37924a7346fbedfa6397dab73bfd1914a2c370
Eleazar Laguna <eleazarlagunam@gmail.com>
2024-05-11 16:09:59 -0600

    add: contacts table charge

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,10 +1,21 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ContactService } from '../../services/contact.service';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+
+interface Contact {
+  id:         number;
+  fullname:   string;
+  created_at: Date;
+  updated_at: Date;
+  deleted:    number;
+  deleted_at: null;
+}
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [MatSelectModule, MatInputModule],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
@@ -12,9 +23,20 @@ export class ContactsComponent implements OnInit {
 
   contactService = inject(ContactService);
 
+  contacts: Contact[] = [];
+
   currentPage: number = 1;
+  lastPage: number = 1;
+
   pageSize: number = 10;
   filters: any = {};
+
+  
+  tableState = {
+    isLoading: true,
+    error: false,
+    errorMessage: 'Loading...'
+  };
 
 
   ngOnInit(): void {
@@ -22,16 +44,30 @@ export class ContactsComponent implements OnInit {
   }
 
   loadContacts(){
+    this.tableState.isLoading = true;
     this.contactService.getContacts(this.currentPage, this.pageSize, this.filters).subscribe({
       next: (response) => {
-        console.log(response);
+        this.lastPage = response.last_page;
+        this.contacts = response.data;
+        this.tableState.isLoading = false;
       },
       error: (error) => {
         console.log(error);
-        
+        this.tableState.isLoading = false;
+        this.tableState.error = true;
+        this.tableState.errorMessage = 'No se pudo procesar su petición';
       }
     });
   }
+
+  onPageSizeChange(newSize: number) {
+    this.loadContacts();
+  }
+
+  // onPageSizeChange(newSize: number) {
+  //   this.pageSize = newSize;
+  //   this.loadContacts();
+  // }
 
 
 
