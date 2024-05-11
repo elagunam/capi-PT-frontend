05e921f6973886bb8712d576c6dd4c7991c295b4
Eleazar Laguna <eleazarlagunam@gmail.com>
2024-05-11 14:45:04 -0600

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ContactsComponent
+    }
+
+
+    // {
+    //     path: 'auth',
+    //     canActivate: [publicGuard],
+    //     loadChildren: () => import('./auth/auth.routes').then(routes => routes.routes)
+    //   }
+];
